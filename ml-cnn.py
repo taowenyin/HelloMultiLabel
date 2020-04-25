d7ed3f9aae72ff2c24bfc9d35a7de48688b82958
taowenyin <wenyin.tao@163.com>
2020-04-25 22:20:59 +0800

    ML-CNN模型搭建

--- ml-cnn.py
+++ ml-cnn.py
@@ -47,7 +47,17 @@ if __name__ == '__main__':
     # 保存损失平均值
     losses_mean = []
     # 每批数据的大小
-    batch_size = 1000
+    batch_size = 100
+
+    # 训练时的损失函数
+    record_train_loss = []
+    # 验证时的损失值
+    record_val_loss = []
+    # 测试时的损失值
+    record_test_loss = []
+
+    # 预测结果
+    predict = []
 
     # 随机创建一组多标签训练数据集
     for i in range(10000):
@@ -115,20 +125,21 @@ if __name__ == '__main__':
     criterion = nn.MultiLabelSoftMarginLoss()
 
     # 设置迭代次数
-    epochs = 5
+    epochs = 50
     # 开始循环迭代
     for epoch in range(epochs):
-        # 保存训练过程的损失函数
-        losses = []
-        for i, sample in enumerate(train):
-            # 创建Tensor对象
-            sample_x = torch.tensor(sample, dtype=torch.float, requires_grad=True).view(1, -1)
-            label_y = torch.tensor(labels[i], dtype=torch.float, requires_grad=True).view(1, -1)
-
-            # 创建分类器，并运行一次前向网络
-            output = clf(sample_x)
+        # 训练时的损失值
+        train_loss = []
+        # 验证时的损失值
+        val_loss = []
+        # 通过enumerate获取每批次索引进行训练
+        for batch_index, (data, target) in enumerate(train_loader):
+            # 标记模型在训练集上训练
+            clf.train()
+            # 使用模型运行一次前向网络
+            output = clf(data)
             # 输出和标签进行对比
-            loss = criterion(output, label_y)
+            loss = criterion(output, target)
 
             # 清空梯度
             optimizer.zero_grad()
@@ -136,14 +147,56 @@ if __name__ == '__main__':
             loss.backward()
             # 一步随机梯度下降
             optimizer.step()
-            # 添加损失均值
-            losses.append(loss.data.mean())
 
-        loss_mean = np.mean(losses)
-        losses_mean.append(loss_mean)
-        print('[{:d}/{:d}] Loss: {:.3f}'.format((epoch + 1), epochs, loss_mean))
+            # 保存训练的损失值
+            train_loss.append(loss.data)
+
+            # 每训练10批就进行一次验证
+            if batch_index % 10 == 0:
+                # 标记模型在验证集上
+                clf.eval()
+                val_loss_arr = []
+                for (val_data, val_target) in validation_loader:
+                    output = clf(val_data)
+                    # 输出和标签进行对比
+                    one_loss = criterion(output, val_target)
+                    # 保存验证集误差
+                    val_loss_arr.append(one_loss.data)
+
+                # 保存验证集的损失平均值
+                val_loss_sub_mean = np.mean(val_loss_arr)
+                val_loss.append(val_loss_sub_mean)
+
+        # 获取每轮迭代的平均损失
+        train_loss_mean = np.mean(train_loss)
+        val_loss_mean = np.mean(val_loss)
+        record_train_loss.append(train_loss_mean)
+        record_val_loss.append(val_loss_mean)
+
+        print('[{:d}/{:d}] Train Loss: {:.3f} Validation Loss: {:.3f}'.format(
+                    (epoch + 1), epochs, train_loss_mean, val_loss_mean))
+
+    # 在测试集测试
+    clf.eval()
+    for (test_data, test_target) in test_loader:
+        predict = clf(test_data)
+        # 输出和标签进行对比
+        test_loss = criterion(predict, test_target)
+        record_test_loss.append(test_loss.data)
+    print('Test Mean Loss: {:.3f}'.format(np.mean(record_test_loss)))
+
+    # 把大于0的设为1
+    predict[predict > 0] = 1
+    predict[predict < 0] = 0
+    predict = predict.detach().numpy().astype(np.int)
 
     # 绘制图像
-    plt.plot(range(len(losses_mean)), losses_mean)
+    plt.plot(range(len(record_train_loss)), record_train_loss, label='Train Loss')
+    plt.plot(range(len(record_val_loss)), record_val_loss, label='Validation Loss')
+    plt.text(30, 0.45, 'Test Mean Loss: {:.3f}'.format(np.mean(record_test_loss)),
+             bbox=dict(boxstyle='round,pad=0.5', fc='yellow', ec='k',lw=1 ,alpha=0.5))
     plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend()
+    plt.title('MultiLabel CNN')
     plt.show()
